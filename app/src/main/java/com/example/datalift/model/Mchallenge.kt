185e8d4b3ab017307e353db11dd5363eb23ba61c
theDeveloperShoon <seancotter04@gmail.com>
2025-01-06 00:53:13 -0500

    Changed Date from number to Int, for now

--- app/src/main/java/com/example/datalift/model/Mchallenge.kt
+++ app/src/main/java/com/example/datalift/model/Mchallenge.kt
@@ -5,6 +5,6 @@ data class Mchallenge(
     private val details: String,
     private val members: List<Muser>,
     private val owner: Muser,
-    private val date: Number,//fix this to be a date object
+    private val date: Int,//fix this to be a date object
     private val requests: List<Muser>//may need to change this to a specific request object
 )
