5fdd99d88a7ed9036d4aae91db37ecb40a955a3e
theDeveloperShoon <seancotter04@gmail.com>
2025-01-06 00:52:20 -0500

    Added Navigation Destinations for navigation

---
+++ app/src/main/java/com/example/datalift/DataliftNavigation.kt
@@ -0,0 +1,11 @@
+package com.example.datalift
+
+object DataliftDestinations {
+    const val LOGIN = "signin"
+    const val SIGNUP = "signin/signup"
+    const val HOME = "home"
+
+}
+
+class DataliftNavigation {
+}
