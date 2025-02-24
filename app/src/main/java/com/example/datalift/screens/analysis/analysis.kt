afe37f49b8cca1f7c43510a99d22680aba17ad5b
Sean Cotter <Seancotter04@gmail.com>
2025-02-24 14:14:18 -0500

    Pushing Analysis code

--- app/src/main/java/com/example/datalift/screens/analysis/analysis.kt
+++ app/src/main/java/com/example/datalift/screens/analysis/analysis.kt
@@ -1,8 +1,14 @@
 package com.example.datalift.screens.analysis
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.patrykandpatrick.vico.compose.cartesian.CartesianChartHost
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberBottom
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberStart
@@ -11,10 +17,64 @@ import com.patrykandpatrick.vico.compose.cartesian.rememberCartesianChart
 import com.patrykandpatrick.vico.core.cartesian.axis.HorizontalAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModelProducer
+import com.patrykandpatrick.vico.core.cartesian.data.lineSeries
+
+
+// Displays two different charts
+// Displays Workout Progression
+    // x-axis time, y-axis = Progression Value
+
+// Displays Exercise Progression
+    // x-axis time, y-axis = Progression Value
+
+// Workout Progression averages progression
+//
+@Composable
+fun AnalysisScreen(
+    analysisViewModel: analysisViewModel = viewModel()
+){
+//    val loading = analysisViewModel.loading.collectAsStateWithLifecycle().value
+    val uiState = analysisViewModel.uiState.collectAsStateWithLifecycle().value
+
+    AnalysisScreen(
+        uiState = uiState,
+        placeholder = {},
+        modifier = Modifier
+    )
+}
 
 @Composable
-fun AnalysisScreen(){
-    Text("Analysis Screen")
+internal fun AnalysisScreen(
+    uiState: AnalysisUiState,
+    placeholder: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val modelProducer = remember { CartesianChartModelProducer() }
+
+    LaunchedEffect(Unit) {
+        modelProducer.runTransaction {
+            lineSeries { series(13, 8, 7, 12, 0, 1, 15, 14, 0, 11, 6, 12, 0, 11, 12, 11) }
+        }
+    }
+
+
+    LazyColumn {
+        item{
+            Text("Analysis Screen")
+        }
+        when(uiState) {
+            AnalysisUiState.Loading -> item {
+
+            }
+            AnalysisUiState.Error -> TODO()
+            is AnalysisUiState.Success -> {
+                item {
+                    ComposeBasicLineChart(modelProducer, modifier = modifier.fillMaxWidth())
+                }
+            }
+        }
+    }
+
 }
 
 @Composable
