64d873e87b85918a1dfa8d485fcc827ff74d3659
Sean Cotter <Seancotter04@gmail.com>
2025-02-24 23:56:30 -0500

    Removed unused import directives

--- app/src/main/java/com/example/datalift/screens/analysis/analysisViewModel.kt
+++ app/src/main/java/com/example/datalift/screens/analysis/analysisViewModel.kt
@@ -1,28 +1,19 @@
 package com.example.datalift.screens.analysis
 
 import android.util.Log
-import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.datalift.model.Manalysis
 import com.example.datalift.model.MexerAnalysis
-import com.example.datalift.model.Mworkout
 import com.example.datalift.model.analysisRepo
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlin.math.log
 
 class analysisViewModel(
 //    private val analysisRepo: analysisRepo,
