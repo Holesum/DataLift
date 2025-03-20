b95f2db70b74f4c0433a262fd3985c3e2ca62673
Sean Cotter <Seancotter04@gmail.com>
2025-03-19 22:34:36 -0400

    Hilt Android added

--- app/src/main/java/com/example/datalift/MainActivity.kt
+++ app/src/main/java/com/example/datalift/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import com.example.datalift.ui.DataliftApp
 import com.example.datalift.ui.rememberDataliftAppState
 import com.example.datalift.ui.theme.DataliftTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             val appState = rememberDataliftAppState()
 
