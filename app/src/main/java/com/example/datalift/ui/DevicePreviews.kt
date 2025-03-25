e3c008754ea15423549851bf64d0b6e92ae5a7d8
Sean Cotter <Seancotter04@gmail.com>
2025-03-25 13:08:30 -0400

    Remove landscape preview

--- app/src/main/java/com/example/datalift/ui/DevicePreviews.kt
+++ app/src/main/java/com/example/datalift/ui/DevicePreviews.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview(device = "spec:width=411dp,height=891dp,dpi=420,isRound=false,chinSize=0dp,orientation=portrait,cutout=none,navigation=gesture")
 
 @Preview(name = "phone", device = "spec:width=360dp,height=640dp,dpi=480")
-@Preview(name = "landscape", device = "spec:width=640dp,height=360dp,dpi=480")
+//@Preview(name = "landscape", device = "spec:width=640dp,height=360dp,dpi=480")
 @Preview(name = "foldable", device = "spec:width=673dp,height=841dp,dpi=480")
 @Preview(name = "tablet", device = "spec:width=1280dp,height=800dp,dpi=480")
 annotation class DevicePreviews
