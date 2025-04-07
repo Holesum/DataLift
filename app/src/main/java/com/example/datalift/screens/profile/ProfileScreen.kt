de38cd7f7498a0a2e0b7623b25d0140a31bd52c9
Sean Cotter <Seancotter04@gmail.com>
2025-04-07 05:32:14 -0400

    Created ProfileScreen.kt to to be next thing to be worked on

---
+++ app/src/main/java/com/example/datalift/screens/profile/ProfileScreen.kt
@@ -0,0 +1,23 @@
+package com.example.datalift.screens.profile
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import com.example.datalift.ui.DevicePreviews
+import com.example.datalift.ui.theme.DataliftTheme
+
+@Composable
+fun ProfileScreen(
+
+){
+    
+}
+
+@DevicePreviews
+@Composable
+fun ProfileScreenPreview(){
+    DataliftTheme {
+        Surface() {
+            ProfileScreen()
+        }
+    }
+}
