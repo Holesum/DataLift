6ecc76cf7a22d1be404cdeb3448e83f8259530a2
Sean Cotter <Seancotter04@gmail.com>
2025-04-08 12:16:38 -0400

    Removed unused import directives

--- app/src/main/java/com/example/datalift/screens/friends/FriendsViewModel.kt
+++ app/src/main/java/com/example/datalift/screens/friends/FriendsViewModel.kt
@@ -18,7 +18,6 @@ import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
