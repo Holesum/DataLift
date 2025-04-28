ac91bafc74ba0a864df62c0b2fdc4314e790d9ca
DCSmith <dcsmith0396@gmail.com>
2025-04-28 00:30:33 -0400

    challenge extension stuffs

--- app/src/main/java/com/example/datalift/data/repository/ChallengeRepository.kt
+++ app/src/main/java/com/example/datalift/data/repository/ChallengeRepository.kt
@@ -5,4 +5,6 @@ import com.example.datalift.model.Mchallenge
 interface ChallengeRepository {
     fun createChallenge(uid: String, challenge: Mchallenge, callback: (Mchallenge?) -> Unit)
     fun getChallengesForUser(uid: String, callback: (List<Mchallenge>) -> Unit)
+    fun deleteChallenge(uid: String, challengeId: String, callback: (Boolean) -> Unit)
+    fun updateChallenge(uid: String, challengeId: String, challenge: Mchallenge, callback: (Boolean) -> Unit)
 }
--- app/src/main/java/com/example/datalift/model/challengeRepo.kt
+++ app/src/main/java/com/example/datalift/model/challengeRepo.kt
@@ -2,6 +2,7 @@ package com.example.datalift.model
 
 import android.util.Log
 import com.example.datalift.data.repository.ChallengeRepository
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.toObject
 import javax.inject.Inject
@@ -10,9 +11,7 @@ class challengeRepo @Inject constructor() : ChallengeRepository {
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     override fun createChallenge(uid: String, challenge: Mchallenge, callback: (Mchallenge?) -> Unit) {
-        val challengeRef = db.collection("Users")
-            .document(uid)
-            .collection("Challenges")
+        val challengeRef = db.collection("Challenges")
 
         // Add the challenge to generate a document ID
         challengeRef.add(challenge)
@@ -23,6 +22,7 @@ class challengeRepo @Inject constructor() : ChallengeRepository {
                 challengeRef.document(docRef.id).set(challengeWithID)
                     .addOnSuccessListener {
                         Log.d("Firebase", "Challenge created with ID: ${docRef.id}")
+                        addUserToChallenge(uid, docRef.id) {}
                         callback(challengeWithID)
                     }
                     .addOnFailureListener { e ->
@@ -50,4 +50,77 @@ class challengeRepo @Inject constructor() : ChallengeRepository {
                 callback(emptyList())
             }
     }
+
+    fun getChallenge(challengeId: String, callback: (Mchallenge?) -> Unit) {
+        db.collection("Challenges")
+            .document(challengeId)
+            .get()
+
+    }
+
+    override fun deleteChallenge(uid: String, challengeId: String, callback: (Boolean) -> Unit) {
+        getChallenge(challengeId){
+            for(user in it?.participants ?: emptyList()){
+                removeRef(uid, challengeId){}
+            }
+            db.collection("Challenges")
+                .document(challengeId)
+                .delete()
+                .addOnSuccessListener {
+                    callback(true)
+                }
+                .addOnFailureListener { e ->
+                    Log.e("ChallengeRepo", "Failed to delete challenge: ${e.message}")
+                    callback(false)
+                }
+        }
+    }
+
+    private fun removeRef(uid: String, challengeId: String, callback: (Boolean) -> Unit){
+        db.collection("Users").document(uid).collection("Challenges").document(challengeId).delete()
+    }
+
+    override fun updateChallenge(uid: String, challengeId: String, challenge: Mchallenge, callback: (Boolean) -> Unit) {
+        db.collection("Users")
+            .document(uid)
+            .collection("Challenges")
+            .document(challengeId)
+            .set(challenge)
+    }
+
+    fun evaluateChallenges(uid: String, callback: (List<Mchallenge>) -> Unit) {
+
+    }
+
+    //Add user to challenge
+    fun addUserToChallenge(uid: String, challengeId: String, callback: (Boolean) -> Unit) {
+        db.collection("Challenges").document(challengeId).update("users", FieldValue.arrayUnion(uid))
+            .addOnSuccessListener {
+                callback(true)
+            }
+            .addOnFailureListener { e ->
+                Log.e("ChallengeRepo", "Failed to add user to challenge: ${e.message}")
+                callback(false)
+            }
+
+        val challengeRef = db.collection("Challenges").document(challengeId)
+
+        db.collection("Users").document(uid)
+            .collection("Challenges")
+            .document(challengeId)
+            .set(mapOf("challengeRef" to challengeRef))
+            .addOnSuccessListener {
+                callback(true)
+            }
+            .addOnFailureListener { e ->
+                Log.e("ChallengeRepo", "Failed to add user to challenge: ${e.message}")
+                callback(false)
+            }
+    }
+
+    //Remove user from challenge
+
+    //
+
+
 }
