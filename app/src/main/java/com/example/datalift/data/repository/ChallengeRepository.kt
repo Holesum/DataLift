2dd48cc93d27404481de74115651b325e7534db0
DCSmith <dcsmith0396@gmail.com>
2025-05-01 14:17:37 -0400

    massive challenge fix along with goal bug fixes

--- app/src/main/java/com/example/datalift/data/repository/ChallengeRepository.kt
+++ app/src/main/java/com/example/datalift/data/repository/ChallengeRepository.kt
@@ -1,6 +1,7 @@
 package com.example.datalift.data.repository
 
 import com.example.datalift.model.Mchallenge
+import com.example.datalift.model.Mworkout
 import kotlinx.coroutines.flow.StateFlow
 
 interface ChallengeRepository {
@@ -9,4 +10,7 @@ interface ChallengeRepository {
     fun getChallengesForCurrentUser(): StateFlow<List<Mchallenge>>
     fun deleteChallenge(uid: String, challengeId: String, callback: (Boolean) -> Unit)
     fun updateChallenge(uid: String, challengeId: String, challenge: Mchallenge, callback: (Boolean) -> Unit)
+    fun addUserToChallenge(uid: String, challengeId: String, callback: (Boolean) -> Unit)
+    fun getChallenge(challengeId: String, callback: (Mchallenge?) -> Unit)
+    fun evaluateChallenges(uid: String, workouts: List<Mworkout> = emptyList(), onComplete: () -> Unit)
 }
